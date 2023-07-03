26e740c5bcc6b8d184a011609a8b169856985abf
alpbrace <ubeyde.alpkilic25@gmail.com>
2023-07-03 21:03:52 +0200

    new

---
+++ Assigments/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "manuel lorenz",
+          phone: "01010101011",
+          email: "ada",
+        },
+        {
+          id: "manuel",
+          name: "manuel lorenz",
+          phone: "01010101011",
+          email: "ada",
+        },
+        {
+          id: "manuel",
+          name: "manuel lorenz",
+          phone: "01010101011",
+          email: "ada",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+
+</style>
---
+++ Assigments/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <h2>
+      {{ friend.name }}
+    </h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:{{ friend.phone }}</strong>
+      </li>
+      <li>
+        <strong>Phone:{{ friend.email }}</strong>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "manuel lorenz",
+        phone: "01010101011",
+        email: "ada",
+      },
+    };
+  },
+
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
